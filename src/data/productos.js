8819dac79043519f549520896fb8f4c6a03b8700
Walter Torres Azorsa <wagetoaz@gmail.com>
2025-11-16 16:42:38 -0500

    añadido productos

---
+++ src/data/productos.js
@@ -0,0 +1,64 @@
+export const productos = [
+  {
+    slug: "laptop-hp-15",
+    name: "Laptop HP 15",
+    price: 2499,
+    image: "https://images.unsplash.com/photo-1496181133206-80ce9b88a853?w=800",
+    description:
+      "Laptop potente ideal para trabajo y estudio con procesador Intel Core i5, 8GB RAM y 256GB SSD.",
+    specs: [
+      "Procesador Intel Core i5",
+      "8GB RAM DDR4",
+      "256GB SSD",
+      'Pantalla 15.6" Full HD',
+      "Windows 11 Home",
+    ],
+    inStock: true,
+  },
+  {
+    slug: "mouse-logitech",
+    name: "Mouse Logitech",
+    price: 79,
+    image: "https://images.unsplash.com/photo-1527864550417-7fd91fc51a46?w=800",
+    description:
+      "Mouse inalámbrico ergonómico con conexión Bluetooth y hasta 12 meses de batería.",
+    specs: [
+      "Conexión Bluetooth",
+      "Batería: 12 meses",
+      "Diseño ergonómico",
+      "Compatible con Windows y Mac",
+    ],
+    inStock: true,
+  },
+  {
+    slug: "teclado-mecanico",
+    name: "Teclado Mecánico",
+    price: 250,
+    image: "https://images.unsplash.com/photo-1587829741301-dc798b83add3?w=800",
+    description:
+      "Teclado mecánico RGB para gaming con switches Cherry MX y retroiluminación personalizable.",
+    specs: [
+      "Switches Cherry MX Red",
+      "Retroiluminación RGB",
+      "Cable trenzado desmontable",
+      "Reposamuñecas incluido",
+    ],
+    inStock: false,
+  },
+  {
+    slug: "monitor-27",
+    name: 'Monitor 27"',
+    price: 850,
+    image: "https://images.unsplash.com/photo-1527443224154-c4a3942d3acf?w=800",
+    description:
+      "Monitor IPS de 27 pulgadas con resolución QHD ideal para diseño y productividad.",
+    specs: [
+      "Resolución 2560x1440 (QHD)",
+      "Panel IPS",
+      "Frecuencia 75Hz",
+      "HDR10",
+      "Puertos HDMI y DisplayPort",
+    ],
+    inStock: true,
+  },
+];
